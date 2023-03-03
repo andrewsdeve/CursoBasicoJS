9d758b84ea0732cff13f20d2f0f130ccd420aec9
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2023-03-03 11:59:20 -0500

    Variables

---
+++ Variables.js
@@ -0,0 +1,21 @@
+/* Que es una Variable: es la representacion de un lugar qaue reservamos en memoria para guardar un valor, el valor puede ser cualquier tipo de dato inclusive objetos o funciones */
+//Ejemplo:
+
+var nombre = "Carlos Martinez"; // Sintaxis para hacer una variable
+
+// Hay dos estados de una variable
+
+var edad; // aqui solo se esta declarando la variable despues se va a dar su valor
+edad = 25; // Aqui se inicializo la variable ya se puede utilizar
+
+// declarando e inicializando una variable
+
+var varlor_1 = 15; // Aqui se asigno y se iniciaalizo
+var elementos = ['Juan','Pedro']; //Variable tipo array
+var persona = { // Variablle tipo objeto
+    nombre: 'Carlos',
+    edad: 30
+}
+console.log(elementos);
+console.log(persona);
+
